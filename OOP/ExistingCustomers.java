36f338cb7bfde3e767c698febdc18dc58812b0dd
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-10-22 20:29:54 +0100

    ExistingCustomers

--- OOP/ExistingCustomers.java
+++ OOP/ExistingCustomers.java
@@ -1,15 +1,15 @@
 package ATM;
 
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 final public class ExisitingCustomers {
 	
 		static Scanner X = new Scanner (System.in);
 		private static double Balance[] = {111, 222, 333, 444, 555, 666};
-		final private static int[] ID = {01, 02, 03, 04, 05, 06};
+		final private static int ID [] = {01, 02, 03, 04, 05, 06};
 		private static String names [] = {"Joshua", "Alex", "Younis", "Eskand", "Ann", "Appul"};
 		private static int Pin [] = {1111, 2222, 3333, 4444, 5555, 6666};
 		private static int Counter = 3;
@@ -18,29 +18,11 @@ final public class ExisitingCustomers {
 		private static int index;
 		private static int CustomerWithdaw;
 		private static int CustomerDeposite;
-		/*
-		public static void NamesSearch() {
-			String names [] = {"Joshua", "Alex", "Younis", "Eskand", "Ann", "Appul"};
-			
-			System.out.println("Please enter a name that you want to search in the system");
-			Scanner search = new Scanner(System.in);
-			Pattern pattern1 = Pattern.compile(names [6] , Pattern.CASE_INSENSITIVE);
-		    Matcher matcher = pattern1.matcher(search.nextLine());
-		    boolean matchFound = matcher.find();
-		    if(matchFound) {
-		      System.out.println("Match found in the existing customers in the array");
-		    } else {
-		      System.out.println("Match not found");
-		    }
-		}
-		
-		*/
-		
+		private static int ChangePIN;
 		
 		final public static void main(String [] args) {
 			PrintCustomers();
 			LogInMethod ();
-			
 		}
 		
 		final public static void PrintCustomers() {
@@ -49,7 +31,7 @@ final public class ExisitingCustomers {
 			System.out.println("Names\t\tBalances\tIDs\t\tPINs");
 			System.out.println("--------------------------------------------------------");
 			for(int i = 0; i < 6; i++) {
-				System.out.println(names[i] + "\t\t" + Balance[i] + "\t\t" + ID[i] + "\t\t" + Pin[i]);
+				System.out.println(names[i] + "\t\t" + Balance[i]+"$" + "\t\t" + ID[i] + "\t\t" + Pin[i]);
 				
 			}
 			System.out.println("--------------------------------------------------------");
@@ -73,22 +55,20 @@ final public class ExisitingCustomers {
 					LogInMethod();
 				}
 			}
-			
 		}
 		
 		private static void SelectionMenu() {
 			
 			while(true) {
-				System.out.println("Select one of the following:\n1.Bank Statement\n2.Withdraw\n3.Deposite\nBack to the main menu\n5.Exit\n");
+				System.out.println("Select one of the following:\n1.Bank Statement\n2.Withdraw\n3.Deposite\n4.Back to the main menu\n5.Exit\n6.Change your PIN?");
 				toInt = X.next();
-				inputValidation();
-				int Y = Integer.parseInt(toInt);
-				switch(Y) {
-				case 1 : StatementMethod();
-				case 2 : WithdrawMethod();
-				case 3 : DepositeMethod();
-				case 4 : BankDemo.TheMainMethodIsHere();
-				case 5 : System.exit(0);
+				switch(toInt) {
+				case "1" : StatementMethod();
+				case "2" : WithdrawMethod();
+				case "3" : DepositeMethod();
+				case "4" : BankDemo.TheMainMethodIsHere();
+				case "5" : System.exit(0);
+				case "6" : changePin();
 				}
 			}
 			
@@ -101,11 +81,8 @@ final public class ExisitingCustomers {
 				CustomerDeposite = Integer.parseInt(toInt);
 				//update user account balance after depositing
 				Balance[index]= Balance[index]+ CustomerDeposite;
-				System.out.println("\nPlease take your reciept\n");
 				//print the updating balance for the customer so they know 
-				System.out.println("\nYour new balance is now: "+ Balance[index]);
-				System.out.println("Your card is still active in the ATM\n"
-				 		+ "Please select from the menu down below\n");
+				System.out.println("Your new balance is now: "+ Balance[index]+"$");
 				SelectionMenu();
 			}//end method
 		
@@ -114,20 +91,20 @@ final public class ExisitingCustomers {
 			
 			System.out.println("\t\tName\t\tPin\t\tBalance\t\tID"
 					+ "\n\t\t---------------------------------------------------");
-			System.out.println("\t\t" + names[index] + "\t\t" + Pin[index] + "\t\t" + Balance[index] + "\t\t" + ID[index]);
+			System.out.println("\t\t" + names[index] + "\t\t" + Pin[index] + "\t\t" + Balance[index]+"$" + "\t\t" + ID[index]);
 			SelectionMenu();
 		}
 
 		private static void WithdrawMethod() {
+			int Q = 0;
 			
 			System.out.println("How much do you want to withdraw?");
 			toInt = X.next();
 			inputValidation();
 			CustomerWithdaw = Integer.parseInt(toInt);
-			if(Balance[index] <= CustomerWithdaw) {
+			if(Balance[index] >= Q) {
 				Balance[index] = Balance[index] - CustomerWithdaw;
-				System.out.println("Your new Balance is: "+Balance[index]);
-				System.out.println("Select from the menu down below");
+				System.out.println("Your new Balance is: "+Balance[index]+"$");
 				SelectionMenu();
 			}
 			else {
@@ -155,5 +132,54 @@ final public class ExisitingCustomers {
 			}
 		}
 		
+		public static void changePin() {//start changePin method 
+			System.out.println("Please enter your new "
+					+ "PIN");//	Asking the customer to enter their new PIN 
+			toInt = X.next();//take in user input as a string
+			inputValidation();//check if the user input is valid
+			ChangePIN = Integer.parseInt(toInt);//parse String into integer
+			//reset the PIN to zero and taking user input PIN instead and store it in the array 
+					Pin[index]= Pin[index] + ChangePIN - Pin[index];//update pin index
+						 DateTimeFormatter dtf = DateTimeFormatter.ofPattern
+								 ("yyyy/MM/dd HH:mm:ss");//set format for date and time that been imported from the device 
+				    	   LocalDateTime now = LocalDateTime.now();//get the time and the date from the machine  
+					 System.out.println("Your new PIN is now: "+ Pin[index] + 
+							 " This PIN been changed on "
+							 +dtf.format(now));
+					 SelectionMenu();//get the customer to selection Menu in-case for any other process
+		}//end method changePin
+		
 
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
