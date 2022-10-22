dcc85309e7bf1418d14bfdafb3a1c3808ffd3585
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-10-22 20:30:13 +0100

    Transaction

--- OOP/Transaction.java
+++ OOP/Transaction.java
@@ -1,14 +1,16 @@
 package ATM;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Transaction {
-
+	DateTimeFormatter date = DateTimeFormatter.ofPattern
+		("yyyy/MM/dd HH:mm:ss");//set format for date and time that been imported from the device 
+	LocalDateTime now = LocalDateTime.now();
     private String Type;
     private double amount;
-    private long accountNum;
-    private Date date;
+    private long accountNum = Balance.MyRandom();
     private Bank b;
     private Balance balance = new Balance();
 
@@ -22,7 +24,6 @@ public class Transaction {
         this.accountNum = accountNum;
         this.Type = Type;
         this.amount = amount;
-        date = new Date();
         operation();
     }
 
@@ -34,25 +35,23 @@ public class Transaction {
                 return;
             }
 
-            balance = new Balance(b.getBalanceListSize() + 1, amount, date);
+            balance = new Balance(b.ReturningMethodForTheArrayList() + 1, amount);
             b.newAccount(balance);
+            System.out.println("Account is opened on: " + date.format(now)+"\n");
         }
 
         else if (Type.equalsIgnoreCase("withdraw")) {
             balance = b.searchAccount(accountNum);
             if (balance == null) {
-                System.out.println("Account ID not found");
+                System.out.println("Your ID not found");
                 return;
             }
             if (balance.getBalance() < amount) {
                 System.out.println("Insufficient Balance.");
                 return;
             }
-            System.out.println("Balance before transaction:");
-            System.out.println(balance.toString()+"$");
             balance.setBalance(balance.getBalance() - amount);
-            System.out.println("Balance after transaction:\n"
-                    + balance.toString()+"$");
+            System.out.println(balance.toString()+"$");
         }
 
         else if (Type.equalsIgnoreCase("deposit")) {
@@ -61,17 +60,14 @@ public class Transaction {
                 System.out.println("Account not found");
                 return;
             }
-            System.out.println("Balance before transaction:");
-            System.out.println(balance.toString()+"$");
             balance.setBalance(balance.getBalance() + amount);
-            System.out.println("Balance after transaction:\n"
-                    + balance.toString()+"$");
+            System.out.println(balance.toString()+"$");
         }
 
         else if (Type.equalsIgnoreCase("showInfo")) {
             balance = b.searchAccount(accountNum);
             if (balance == null) {
-                System.out.println("Account ID not found");
+                System.out.println("Your ID not found");
                 return;
             }
             System.out.println(balance.toString());
