8da555217958de499c569f602ee3e1c697d53835
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-10-28 17:34:53 +0100

    Bank

--- OOP/Programming/BANK/Bank.java
+++ OOP/Programming/BANK/Bank.java
@@ -1,29 +1,29 @@
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.ArrayList;//importing the arraylist from the utility 
+import java.util.Iterator;//importing the Iterator from the utility 
 //import java.util.Scanner;
 
-public class Bank {
+public class Bank {//start class 
 
-    private ArrayList < Balance > List;
-    Iterator < Balance > itr;
+    private ArrayList < Balance > List;//declaring private array list to store new customers 
+    Iterator < Balance > itr;//declare an iterator to go through and sreach fro specific ID when requested 
     
-    public Bank() {
-        List = new ArrayList < Balance > ();
-    }
+    public Bank() {//start constructor 
+        List = new ArrayList < Balance > ();//assign List variable to the ArrayList 
+    }//end 
 
-    public void newAccount ( Balance e ) {
-       List.add(e);
-    }
+    public void newAccount ( Balance e ) {//start a method from the class Balance with parameter (e) to pass information to the method 
+       List.add(e);//add every (e) to the variable that is holding the ArrayList 
+    }//end the method 
 
-   public Balance searchAccount( long accountNum ) {
-        itr = List.iterator();
-        while ( itr.hasNext () ) {
-            Balance b = new Balance( itr.next() );
-            if ( b.getAccountNum() == accountNum )
-                return b;
+   public Balance searchAccount( long accountNum ) {//start method that calling Balance class instead of static 
+        itr = List.iterator();//get the iterator method to the variable that holding the ArrayList 
+        while ( itr.hasNext () ) {//start loop
+            Balance b = new Balance( itr.next() );//connecting the Balance class and hold it in variable b, itr.next() is a method to keep checking in the ArrayList 
+            if ( b.getAccountNum() == accountNum )//if the iterator assigned to the same value in the ArrayList
+                return b;//then return b
         }
-        return null;
-    }
+        return null;//otherwise return nothing 
+    }//end method 
     
     /*
     public void searchAccountAll(long accountNum) {
@@ -47,7 +47,7 @@ public class Bank {
     	   return List; 
     	} 
     	*/
-    public long ReturningMethodForTheArrayList() {
-        return List.size();
-    }
-}
+    public long ReturningMethodForTheArrayList() {//start method without void 
+        return List.size();//reurning the size of the ArrayList 
+    }//end method 
+}//end class 
