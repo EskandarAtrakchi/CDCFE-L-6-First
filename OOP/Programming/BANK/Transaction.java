4a5e94b63b7b390ce61d7844b11afecc9be37582
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-10-28 18:04:49 +0100

    transac

--- OOP/Programming/BANK/Transaction.java
+++ OOP/Programming/BANK/Transaction.java
@@ -1,42 +1,46 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+import java.time.LocalDateTime;//import the local time from the machine 
+import java.time.format.DateTimeFormatter;//import the format 
+import java.util.Scanner;//import the Scanner from java utility
 
-public class Transaction {
+public class Transaction {//start class 
 	DateTimeFormatter date = DateTimeFormatter.ofPattern
 		("yyyy/MM/dd HH:mm:ss");//set format for date and time that been imported from the device 
 	LocalDateTime now = LocalDateTime.now();
+    //declaring private variables 
     private String Type;
     private double amount;
     private long accountNum;
-    private Bank b;
-    private Balance balance = new Balance();
+    //end of declaring variables 
+    private Bank b;//declaring variable b to assign it to a new Bank with default constructor later 
+    
+    private Balance balance = new Balance();//connecting the two classes together, holded by variable, there is default constructor here 
 
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);//Scanner input streamed as System.in held by sc 
 
-    public Transaction() {
-        b = new Bank();
+    public Transaction() {//start constructor 
+        b = new Bank();//assigning b to new Bank()
     }
 
-    public void transaction(long accountNum, String Type, double amount) {
+    public void transaction(long accountNum, String Type, double amount) {//start method with parameters 
+    //this. is to let the system know that the parameter intended here and not the variables been declared above 
         this.accountNum = accountNum;
         this.Type = Type;
         this.amount = amount;
-        operation();
-    }
+        operation();//calling the method 
+    }//end the method 
 
-    private void operation() {
+    private void operation() {//start metod 
 
-        if (Type.equalsIgnoreCase("Opening")) {
-            if (amount < 0) {
+        if (Type.equalsIgnoreCase("Opening")) {//start if 
+            if (amount < 0) {//start if when customer enters balance less than zero 
                 System.out.println("Opening balance cannot be less than zero.");
                 return;
-            }
+            }//end if 
 
-            balance = new Balance(b.ReturningMethodForTheArrayList() + 1, amount);
+            balance = new Balance(b.ReturningMethodForTheArrayList() + 1, amount);//
             b.newAccount(balance);
             System.out.println("Account is opened on: " + date.format(now)+"\n");
-        }
+        }//end if 
 
         else if (Type.equalsIgnoreCase("withdraw")) {
             balance = b.searchAccount(accountNum);
