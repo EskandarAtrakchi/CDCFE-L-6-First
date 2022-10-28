c8be57e53ac37a82f4bd7305d867a484d23d9528
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-10-28 17:20:04 +0100

    Balance

--- OOP/Programming/BANK/Balance.java
+++ OOP/Programming/BANK/Balance.java
@@ -1,46 +1,46 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+import java.time.LocalDateTime;//importing locatal time from the machine 
+import java.time.format.DateTimeFormatter;//importing the format
+import java.util.Scanner;//importing the Scanner from utility
 
-public class Balance {
+public class Balance {//start class 
 	DateTimeFormatter date = DateTimeFormatter.ofPattern
 		("yyyy/MM/dd HH:mm:ss");//set format for date and time that been imported from the device 
-	LocalDateTime now = LocalDateTime.now();
-    private double balance;
-    private long accountNum;
-    Scanner sc = new Scanner( System.in );
+	LocalDateTime now = LocalDateTime.now();//special method here 
+    private double balance;//private variable 
+    private long accountNum;//private variable 
+    Scanner sc = new Scanner( System.in );//Scanner holds by sc variable 
 
-    public Balance(long aNo, double money) {
-        accountNum = aNo;
-        balance = money;
+    public Balance(long aNo, double money) {//constructor with two variables 
+        accountNum = aNo;//assigning value
+        balance = money;//assigning value
         System.out.println("New account created with account ID: " + accountNum);
         System.out.println("Opening balance is " + balance + "$");
         
+    }//end constructor 
+
+    public Balance(Balance b) {////constructor with two variable to connect it to the other class 
+        balance = b.balance;//assigning value
+        accountNum = b.accountNum;//assigning value
+    }//end constructor 
+
+    public Balance() {}//empty constructor 
+
+    public long getAccountNum() {//get method 
+        return accountNum;//returning value (this accountNum been assigned to b.accountNum and to aNo)
+    }//end get 
+
+    public double getBalance() {//start get 
+        return balance;//retruning value (this value been assigned to money and to b.balance)
     }
 
-    public Balance(Balance b) {
-        balance = b.balance;
-        accountNum = b.accountNum;
+    public void setBalance(double balance) {//start set
+        this.balance = balance;//setting this balance 
     }
 
-    public Balance() {}
-
-    public long getAccountNum() {
-        return accountNum;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public String toString() {
+    public String toString() {//start to sting method 
         return "Your ID Number is: " + accountNum + "\nCurrent balance: " + balance + "$"
-                + "\nLast date of update was on: " + date.format ( now ) + "\n";
+                + "\nLast date of update was on: " + date.format ( now ) + "\n";//returning 
 
-    }
+    }//end toString 
 
-}
+}//end class 
