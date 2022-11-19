e575d82685357386e93fdb3f69cbc1989614aeb2
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-19 14:31:52 +0000

    DB

---
+++ OOP/Programming/ExistingCustomerTries/DB.java
@@ -0,0 +1,15 @@
+
+public class DB {
+
+	static Bank [] Arrays = {
+			
+    	new AbstractFromExistingCustomers (0, "Noah", "1111", 100, 1, 0, 0, 0, 0),
+        new AbstractFromExistingCustomers ("Daniel", "2222", 200, 2),
+        new AbstractFromExistingCustomers ("Caoimhe", "3333", 300, 3),
+        new AbstractFromExistingCustomers ("Sophie", "4444", 400, 4),
+        new AbstractFromExistingCustomers ("Ella", "6543", 500, 5),
+        new AbstractFromExistingCustomers ("Jack", "2109", 600, 6)
+        
+    };
+	
+}
