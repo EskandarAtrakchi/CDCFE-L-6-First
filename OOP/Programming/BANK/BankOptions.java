b60546095aa858ca6103e828d5c5b7303c8fd4d8
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-19 19:03:03 +0000

    BankOptions

--- OOP/Programming/BANK/BankOptions.java
+++ OOP/Programming/BANK/BankOptions.java
@@ -1,13 +1,49 @@
+package ATM;
+
 import java.util.*;
 
 public class BankOptions {
 	static Scanner X = new Scanner (System.in);
     public static void main(String args [ ]) {
-    	TheMainMethodIsHere();
+    	TheMainMethodOfBankOption();
     	
     }
     
-    public static void TheMainMethodIsHere() {
+    public static void TheMainMenu () {
+    	Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("You can choose One of the following:");
+			System.out.println("1.Log in for registered accounts");
+			System.out.println("2.Accounts Search For Bank owners");
+			System.out.println("3.Check temporary account options");
+			System.out.println("4.Exit");
+			
+            String LOOP = sc.next();
+            AbstractFromExistingCustomers CallingMethodsFromAnotherClass = new AbstractFromExistingCustomers();
+			switch(LOOP) {
+			
+			case "1" : CallingMethodsFromAnotherClass.LogInMethod(); 
+			break;
+			
+			case "2" : CallingMethodsFromAnotherClass.SearchCustomers();
+			break;
+			
+			case "3" : TheMainMethodOfBankOption();
+			break;
+			
+			case "4" : System.out.println("Thank you, bye!"); 
+			sc.close();
+			System.exit(0);
+			
+			default:
+                System.out.println("Wrong choice!!"); 
+                break;
+			}
+		}
+		
+	}
+    
+    public static void TheMainMethodOfBankOption() {
     	
     	String choice, ch, operation;
         Transaction pullingInfoFromTransactionClass = new Transaction();
@@ -16,24 +52,25 @@ public class BankOptions {
         long accountNo;
 
         do {
-            System.out.println("1. Create an account?");
-            System.out.println("2. Transaction");
-            System.out.println("3. View Account Information");
+            System.out.println("1. Create Temporary flexible account?");
+            System.out.println("2. Transaction for the temporary created account");
+            System.out.println("3. View the Temporary Account Information");
             System.out.println("4. Existed Customer?");
             System.out.println("5. Exit");
-            System.out.println("6. For DEV use only");
+            System.out.println("6. Click here if you are the owner");
             choice = sc.next();
             switch (choice) {
 
             case "1":
+            	
                 double openingBalance;
-
                 System.out.println("Enter the opening balance :");
                 openingBalance = sc.nextDouble();
                 pullingInfoFromTransactionClass.transaction(0, "Opening", openingBalance);
                 break;
 
             case "2":
+            	
                 System.out.println("1. Deposit");
                 System.out.println("2. Withdraw");
                 ch = sc.next();
@@ -52,6 +89,7 @@ public class BankOptions {
                 break;
 
             case "3":
+            	
                 System.out.println("Your ID Number:");
                 accountNo = sc.nextLong();
                 operation = "showInfo";
@@ -59,26 +97,30 @@ public class BankOptions {
                 break;
                 
             case "4":
-            	ExistingCustomers.PrintCustomers();
-            	ExistingCustomers.LogInMethod (); 
+            	
+            	AbstractFromExistingCustomers Access = new AbstractFromExistingCustomers();
+            	Access.LogInMethod (); 
             	break;
 
             case "5":
-                System.out.println("Thank you, Bye!"); System.exit(0);
+            	
+                System.out.println("Thank you!");
                 break;
+                
+            case "6" :
+            	
+            	TheMainMenu ();
             
-      
-            case "6":
-            	ExistingCustomers.NamesSearch(); break;
-			
             default:
+            	
                 System.out.println("Wrong choice!!"); 
                 break;
             }
-        } while (choice != "5");
+        } 
+        while (choice != "5");
         sc.close();
     }
     static {
-		System.out.println("Welcome to Eskandar's Bank");
-	}
+    	System.out.println("\tThis is a TEST ATM");
+    }
 }
