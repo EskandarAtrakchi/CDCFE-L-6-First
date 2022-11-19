6ea3d19b45361632830764f046a174028700f170
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-19 22:35:49 +0000

    StartExistedCustomers

--- OOP/Programming/BANK/StartExistedCustomers.java
+++ OOP/Programming/BANK/StartExistedCustomers.java
@@ -1,11 +1,11 @@
-package ATM;
+package ATM;//Declaring a packing location 
 
-public class StartExistedCustomers {
+public class StartExistedCustomers {//start class of the main method 
 	
-    public static void main(String [ ] args) {
-    	
+    public static void main(String [ ] args) {//start main method 
+    	//create an object from AbstractFromExistingCustomers class 
     	AbstractFromExistingCustomers Obj = new AbstractFromExistingCustomers();
     	
-    	Obj.LogInMethod();
-    }
-}
+    	Obj.LogInMethod();//calling non static method to static main method 
+    }//end main
+}//end class
