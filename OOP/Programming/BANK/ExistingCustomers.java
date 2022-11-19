69dcd4f527ebe7b70668f575f436dd39965c0abd
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-19 19:03:51 +0000

    StartExistedCustomers

--- OOP/Programming/BANK/ExistingCustomers.java
+++
@@ -1,7 +0,0 @@
-
-
-public class ExistingCustomers extends AbstractFromExistingCustomers {}
-	//AbstractFromExistingCustomers abstraction = new AbstractFromExistingCustomers();
-	//default Constructor is build in the other class
-	//Cannot instantiate the type AbstractFromExistingCustomers
-	//This class connects all to the other classes 
---
+++ OOP/Programming/BANK/StartExistedCustomers.java
@@ -0,0 +1,11 @@
+package ATM;
+
+public class StartExistedCustomers {
+	
+    public static void main(String [ ] args) {
+    	
+    	AbstractFromExistingCustomers Obj = new AbstractFromExistingCustomers();
+    	
+    	Obj.LogInMethod();
+    }
+}
