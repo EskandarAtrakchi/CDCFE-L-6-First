7bd64b03e3b5b084a0fa399b2b5a7534ff7b0950
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-19 19:04:25 +0000

    DB

---
+++ OOP/Programming/BANK/DB.java
@@ -0,0 +1,10 @@
+package ATM;
+
+public class DB extends AbstractFromExistingCustomers {
+
+	public DB (int AccountID, String AccountName, int AccountPin, int AccountBalance)
+	{
+        super(AccountID,AccountName, AccountPin, AccountBalance);
+    }
+
+}
