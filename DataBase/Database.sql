1090d592daf134f0ae6e27a01f04e2acd194861a
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-22 15:25:54 +0000

    Database

---
+++ DataBase/Database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE VEHICLES (
+    VehicleName Varchar NOT NULL,
+    VehicleCpacity int NOT NULL,
+    VehicleColor Varchar NOT NULL,
+    VehiclePrice float NOT NULL,
+    VehicleCondition Varchar NOT NULL,
+    VehicleSize Varchar NOT NULL,
+    VehicleYearModel int NOT NULL,
+    PRIMARY KEY (VehcileID)
+);
+
+INSERT INTO VEHICLES ()
+VALUES ();
