c74a46003d2cbefb102e3483ae7c16778e723943
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-24 20:01:26 +0000

    CurrentDB

---
+++ OOP/Programming/BANK-BRF/Account.java/CurrentDB.java
@@ -0,0 +1,8 @@
+
+public class CurrentDB extends CurrentAccount{
+
+	public CurrentDB(int AccountID, String AccountName, int AccountPin, double AccountBalance) {
+		super(AccountID,AccountName, AccountPin, AccountBalance);
+	}
+
+}
