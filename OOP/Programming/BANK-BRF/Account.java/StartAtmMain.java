3863844dc460c5f698f536183e1a5e8d0c2d0030
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-24 20:00:43 +0000

    StartAtmMain

---
+++ OOP/Programming/BANK-BRF/Account.java/StartAtmMain.java
@@ -0,0 +1,12 @@
+
+public class StartAtmMain {
+
+	public static void main(String[] args) {
+		SavingsAccount SavingsObj = new SavingsAccount();
+		CurrentAccount CurrentObj = new CurrentAccount();
+		
+		CurrentObj.CurrentLogIn ();
+
+	}
+
+}
