7dbcccfd10d525ba825a2925334234af001b442f
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-24 23:41:20 +0000

    Account

--- OOP/Programming/BANK-BRF/Account.java/Account.java
+++ OOP/Programming/BANK-BRF/Account.java/Account.java
@@ -1,11 +1,11 @@
 
 public class Account {
-
+	 
+    private static int Attempts = 3;//declare int variable 
 	protected int AccountID;//declare int variable 
     protected String AccountName;//declare String variable
     protected int AccountPin;//declare int variable 
-    protected double AccountBalance;//declare int variable 
-    private static int Attempts = 3;//declare int variable 
+    protected double AccountBalance;//declare int variable
     protected int ProtectedVarForPIN;//declare int variable 
     
 	public int getAccountID() {
@@ -45,21 +45,17 @@ public class Account {
 	public String toString() {
 		return "Account [AccountID=" + AccountID + ", AccountName=" + AccountName + ", AccountPin=" + AccountPin
 				+ ", AccountBalance=" + AccountBalance + "$" + "]";
-	}
+	}//end toString
 
-	
-	
 	public static void AttemptMethod() {//start method 
 		
 		Attempts--;//decrement the variable value 
 		System.out.println("\nYou have " 
 		+ Attempts + 
 		" attempts left\n");//output the decremented variable 
-
 			if ( Attempts == 0 ) {//start if 
 				System.out.println("Attention your card is blocked");
 				System.exit( 0 );//shutdown the system 
 		}//end if 
 	}//end method 
-	
-}
+}//end class
