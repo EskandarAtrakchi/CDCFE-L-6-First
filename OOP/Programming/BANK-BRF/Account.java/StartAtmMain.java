965dded797657fa0f7a314e5294c8bbd4fb1cff2
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-11-24 22:14:34 +0000

    StartAtmMain

--- OOP/Programming/BANK-BRF/Account.java/StartAtmMain.java
+++ OOP/Programming/BANK-BRF/Account.java/StartAtmMain.java
@@ -1,12 +1,50 @@
+import java.util.Scanner;
 
+/**
+ * @author Eskandar Atrakchi
+ *
+ */
 public class StartAtmMain {
-
+	private static int Attempts = 3;
+	static {
+		System.out.println("This is a test ATM");
+	}
 	public static void main(String[] args) {
+		while(true) {
+		System.out.println("1.SavingsAccounts\n"
+				+ "2.CurrentAccounts\n"
+				+ "3.Exit the ATM");
 		SavingsAccount SavingsObj = new SavingsAccount();
 		CurrentAccount CurrentObj = new CurrentAccount();
+		String LOOP;
+		Scanner X = new Scanner(System.in);
+		LOOP = X.next();//take user input as a String 
 		
-		CurrentObj.CurrentLogIn ();
-
+		switch(LOOP) {//start switch statement
+		case "1" : 
+			SavingsObj.SavingsLogIn ();
+			X.close();
+			break;
+			
+		case "2" : 
+			CurrentObj.CurrentLogIn ();
+			X.close();
+			break;
+			
+		case "3" :
+			System.out.println("Thank you, bye!");
+			X.close();
+			System.exit(0);
+			
+		default:
+			System.out.println("Wrong choice!");
+			Attempts--;//decrement the variable value 
+			if ( Attempts == 0 ) {//start if 
+				System.out.println("Attention too many wrong attempts! try again later!");
+				System.exit( 0 );//shutdown the system 
+		}//end if 
+			break;
+		}
+	   }
 	}
-
 }
